c585cab6ab691847eb7f2e4a53a4025b6c7aa003
Shivkumar Shivaji <sshivaji@gmail.com>
2018-07-29 20:12:04 -0700

    Added new library for React

--- src/index.js
+++ src/index.js
@@ -1,8 +1,70 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Chess from 'chess.js'
+import Chessground from 'react-chessground'
+import 'react-chessground/dist/styles/chessground.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Demo extends React.Component {
+
+  chess = new Chess();
+
+  state = {
+    fen: '',
+    lastMove: null
+  };
+
+  turnColor() {
+    return (this.chess.turn() === 'w') ? 'white' : 'black'
+  }
+
+  calcMovable() {
+    const dests = {};
+    this.chess.SQUARES.forEach(s => {
+      const ms = this.chess.moves({square: s, verbose: true});
+      if (ms.length) dests[s] = ms.map(m => m.to)
+    });
+    return {
+      free: false,
+      dests,
+      // color: this.myColor()
+    }
+  }
+
+  myColor() {
+    return 'white'
+  }
+
+  onMove = (from, to) => {
+    const chess = this.chess;
+    if (chess.move({ from, to, promotion: 'q' })) {
+      this.setState({ fen: chess.fen(), lastMove: [from, to] })
+      // setTimeout(this.randomMove, 500)
+      // this.turnColor()
+    }
+  };
+
+  randomMove = () => {
+    const chess = this.chess;
+    const moves = chess.moves({ verbose: true });
+    const move = moves[Math.floor(Math.random() * moves.length)];
+    chess.move(move.san);
+    this.setState({ fen: chess.fen(), lastMove: [move.from, move.to] })
+  };
+
+  render() {
+    return <Chessground
+      width={512}
+      height={512}
+      orientation={this.myColor()}
+      turnColor={this.turnColor()}
+      movable={this.calcMovable()}
+      lastMove={this.state.lastMove}
+      fen={this.state.fen}
+      onMove={this.onMove}
+      style={{ margin: 'auto' }}
+      coordinates={false}
+    />
+  }
+}
+
+ReactDOM.render(<Demo />, document.getElementById('root'));
